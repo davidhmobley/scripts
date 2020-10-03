3de32aab9b623cae018a6266246b94cecc137b60
David Mobley <davidhmobley@gmail.com>
2020-10-03 13:55:52 -0400

    add read

--- test.sh
+++ test.sh
@@ -4,12 +4,14 @@ STR="/home/david/bin/dhm.sh"
 
 echo ${STR%.sh}    # /path/to/foo
 echo ${STR%/*}      # /path/to
-
 echo ${STR##*.}     # cpp (extension)
 echo ${STR##*/}     # foo.cpp (basepath)
-
 echo ${STR#*/}      # path/to/foo.cpp
 echo ${STR##*/}     # foo.cpp
-
 echo ${STR/foo/bar} # /path/to/bar.cpp
 
+echo " "
+
+echo "Enter name of DB:"
+read dbName
+echo "You entered $dbName"
