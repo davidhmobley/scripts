7184cd6a108dd66e4ec6e1a481326e3e8ff31f92
David Mobley <davidhmobley@gmail.com>
2020-10-11 08:23:54 -0400

    improvements

--- pullAll.sh
+++ pullAll.sh
@@ -1,5 +1,25 @@
 #!/bin/zsh
 
+# line separator
+prettyPrint() {
+    chars=$1
+    firstChar=1 #false
+
+    for ((i = 0 ; i < ${#chars} ; i++)); do
+        # any blanks prefixing the line?
+        if [[ $firstChar -eq 1 ]]; then
+            if [[ ${chars:$i:1} == " " ]]; then
+                printf " " 
+            else
+                firstChar=0 #true
+            fi
+        else
+            printf "*"
+        fi
+    done
+    printf "\n"
+}
+
 SAVEIFS=$IFS
 IFS=$(echo -en "\n\b")
 
@@ -11,10 +31,11 @@ cd ~/StudioProjects
 echo "Pulling in latest changes for all repositories..."
 
 # Find all git repositories and update it to the master latest revision
-for i in $(find . -mindepth 1 -maxdepth 1 -type d -print | cut -c 3-); do
-    echo "";
-    echo "$i";
-    cd "/users/david/StudioProjects/$i";
+for project in $(find . -mindepth 1 -maxdepth 1 -type d -print | cut -c 3-); do
+    echo "$project";
+	prettyPrint "$project"
+	
+    cd ~/StudioProjects/$project
 	
 	# under git control?
 	if [[ -d ".git" ]]; then
@@ -28,6 +49,8 @@ for i in $(find . -mindepth 1 -maxdepth 1 -type d -print | cut -c 3-); do
 	else
 		echo "*** not under git control, skipping"
 	fi	
+
+    echo "";
 done
 
 # lets get back to the CUR_DIR
