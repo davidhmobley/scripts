480c105438ff642dd2233ba9f14a5a474ded183c
David Mobley <davidhmobley@gmail.com>
2020-10-17 08:05:33 -0400

    prune unwanted chars

--- mytest.sh
+++ mytest.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-str="2. Hello {\"a\", \"b\"}"
+str="2. Hello {\"a\"\r, \"b\"\r}"
 echo "Original: $str"
+
+str=$(echo $str | tr -d '\\r')
+echo "Modified: $str"
 
 # separate out JSON from the string
 nonjson=$(echo $str | cut -d'{' -f 1)
