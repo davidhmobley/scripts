a27c4680025ef879dbf64c1cf3832eae0b043d80
David Mobley <davidhmobley@gmail.com>
2020-10-18 11:54:47 -0400

    more changes

--- mytest.sh
+++ mytest.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
-str="2. Hello {\"a\": \"cat\"\r, \"b\": \"dog\"\r}"
+str="2. Hello {\"a\": \"cat\"\r, \"b\": [{\"c\": \"dog\"\r},{\"c\": \"frog\"}]}"
 echo "Original: $str"
-
+echo " "
 str=$(echo $str | tr -d '\\r')
 echo "Modified: $str"
+echo " "
 
 # separate out JSON from the string
-nonjson=$(echo $str | cut -d'{' -f 1)
+nonjson=$(echo $str | cut -d '{' -f 1)
 echo "NonJson: $nonjson"
-json=$(echo $str | cut -d'{' -f 2)
+echo " "
+json=$(echo $str | cut -d '{' -f 2-)
 json="{${json}"
 echo "Json: $json"
+echo " "
 echo "formatted json..."
 echo $json | jq .
